bf269e73e13dd43c4a649023d6c8467621a02e5b
LuisaTorresMon <lj.torresm1@uniandes.edu.co>
2024-03-10 18:20:19 -0500

    Se crea la vista de posponer alarma

--- app/src/main/java/com/example/conectandes/MainActivity.kt
+++ app/src/main/java/com/example/conectandes/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.conectandes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
+        val linearLayout = findViewById<LinearLayout>(R.id.section2)
+
+        linearLayout.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/conectandes/MainActivity2.kt
+++ app/src/main/java/com/example/conectandes/MainActivity2.kt
@@ -1,11 +1,28 @@
 package com.example.conectandes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val posponerAlarmaButton = findViewById<ImageButton>(R.id.section3)
+        val detenerAlarmaButton = findViewById<ImageButton>(R.id.section4)
+
+        posponerAlarmaButton.setOnClickListener {
+            // Define el Intent para iniciar MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        detenerAlarmaButton.setOnClickListener {
+            // Define el Intent para iniciar MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
